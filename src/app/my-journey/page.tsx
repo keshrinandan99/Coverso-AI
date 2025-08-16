49a901b16ab74a4fe041a16c7f5697de2ed97bee
nandan <sameernandan44@gmail.com>
2025-08-17 05:14:51 +0530

    implement navigation bar

--- src/app/my-journey/page.tsx
+++ src/app/my-journey/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function MyJourney() {
   return (
-    <div>MyJourney</div>
+    <div>
+    <h1>MyJourney</h1>
+    </div>
   )
 }
 
---
+++ src/components/Navlinks.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { cn } from "@/lib/utils"
+const navlinks=[
+    {label:"Home",href:"/"},
+    {label:"Companions", href:"/companions"},
+    {label:"My Journey", href:"/my-journey"},
+]
+
+const Navlinks=()=>{
+    const pathname=usePathname();
+    return (
+        <>
+        <div className="flex items-center gap-4">
+        {navlinks.map(({label,href})=>(
+            <Link href={href} 
+            key={label}
+            className={cn(pathname===href && 'text-primary font-semibold')}
+             >{label}</Link>
+        ))}
+        </div>
+
+        </>
+    )
+}
+export default Navlinks
